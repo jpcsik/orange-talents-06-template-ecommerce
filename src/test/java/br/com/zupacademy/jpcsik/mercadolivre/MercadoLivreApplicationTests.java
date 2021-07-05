d1cea5da0979b94a1cb7ac034e24c2bd973b5851
jpcsik <joao.batista@zup.com.br>
2021-07-05 16:24:52 -0300

    Setup do Projeto

---
+++ src/test/java/br/com/zupacademy/jpcsik/mercadolivre/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.jpcsik.mercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
